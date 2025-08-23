7074d425ab927d032ffcdd5742bda72a809f0192
barisgudul <mehmetbarisgudul@gmail.com>
2025-08-24 00:28:24 +0300

    feat: First commit.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,21 @@
+import { FontAwesome } from '@expo/vector-icons'; // Örnek ikon kütüphanesi
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Döviz',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="dollar" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="altin"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Altın',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="database" color={color} />, // Uygun bir ikon bul
         }}
       />
     </Tabs>
--- app/(tabs)/altin.tsx
+++ app/(tabs)/altin.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function DovizScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Döviz Ekranı</Text>
+      <Text>Altın Ekranı</Text>
     </View>
   );
 }
